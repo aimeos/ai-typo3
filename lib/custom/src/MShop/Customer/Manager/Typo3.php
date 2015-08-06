10c8d5396c438d929ad48b07fd3bd1b4d6e4077a
Aimeos <aimeos@aimeos.org>
2015-08-06 09:39:07 +0200

    Adapt to search key change for verified date in customer manager

--- lib/custom/src/MShop/Customer/Manager/Typo3.php
+++ lib/custom/src/MShop/Customer/Manager/Typo3.php
@@ -187,9 +187,9 @@ class MShop_Customer_Manager_Typo3
 			'type' => 'integer',
 			'internaltype' => MW_DB_Statement_Abstract::PARAM_INT
 		),
-		'customer.vdate'=> array(
+		'customer.dateverified'=> array(
 			'label' => 'Customer verification date',
-			'code' => 'customer.vdate',
+			'code' => 'customer.dateverified',
 			'internalcode' => 't3feu."vdate"',
 			'type' => 'date',
 			'internaltype' => MW_DB_Statement_Abstract::PARAM_STR,
