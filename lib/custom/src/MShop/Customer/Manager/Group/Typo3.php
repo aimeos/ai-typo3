71aef23441a0ccc9662da18ad9e8ebd552f8df95
Aimeos <aimeos@aimeos.org>
2015-08-11 12:27:52 +0200

    Customer group manager for TYPO3

---
+++ lib/custom/src/MShop/Customer/Manager/Group/Typo3.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015
+ * @package MShop
+ * @subpackage Customer
+ */
+
+
+/**
+ * TYPO3 implementation of the customer group class
+ *
+ * @package MShop
+ * @subpackage Customer
+ */
+class MShop_Customer_Manager_Group_Typo3
+	extends MShop_Customer_Manager_Group_Default
+	implements MShop_Customer_Manager_Group_Interface
+{
+}
