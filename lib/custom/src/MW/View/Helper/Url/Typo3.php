3d316016d81055bdf8846a97a2080e260844bb14
Aimeos <aimeos@aimeos.org>
2015-12-12 17:12:53 +0100

    Use view helper interface

--- lib/custom/src/MW/View/Helper/Url/Typo3.php
+++ lib/custom/src/MW/View/Helper/Url/Typo3.php
@@ -3,7 +3,7 @@
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
  * @copyright Metaways Infosystems GmbH, 2012
- * @copyright Aimeos (aimeos.org), 2014
+ * @copyright Aimeos (aimeos.org), 2014-2015
  * @package MW
  * @subpackage View
  */
@@ -20,7 +20,7 @@ namespace Aimeos\MW\View\Helper\Url;
  */
 class Typo3
 	extends \Aimeos\MW\View\Helper\Base
-	implements \Aimeos\MW\View\Helper\Iface
+	implements \Aimeos\MW\View\Helper\Url\Iface
 {
 	private $uriBuilder;
 	private $prefix;
