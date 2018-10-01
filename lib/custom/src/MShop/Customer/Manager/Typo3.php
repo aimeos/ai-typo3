d5f439033ccf7e31e6e5a416c4b6884e35097877
Aimeos <aimeos@aimeos.org>
2018-10-01 10:05:24 +0200

    Added value parameter for initializing item in createItem()

--- lib/custom/src/MShop/Customer/Manager/Typo3.php
+++ lib/custom/src/MShop/Customer/Manager/Typo3.php
@@ -306,11 +306,14 @@ class Typo3
 	 *
 	 * @param string|null Type the item should be created with
 	 * @param string|null Domain of the type the item should be created with
+	 * @param array $values Values the item should be initialized with
 	 * @return \Aimeos\MShop\Common\Item\Site\Iface New site item object
 	 */
-	public function createItem( $type = null, $domain = null )
+	public function createItem( $type = null, $domain = null, array $values = [] )
 	{
-		$values = ['customer.siteid' => $this->getContext()->getLocale()->getSiteId(), 'typo3.pageid' => $this->pid];
+		$values['customer.siteid'] = $this->getContext()->getLocale()->getSiteId();
+		$values['typo3.pageid'] = $this->pid;
+
 		return $this->createItemBase( $values );
 	}
 
