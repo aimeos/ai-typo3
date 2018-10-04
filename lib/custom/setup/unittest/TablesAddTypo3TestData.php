6b9391cb599a00802c9a042c27a90d53bb68c5de
Aimeos <aimeos@aimeos.org>
2018-10-04 11:04:19 +0200

    Fixed setup task dependencies

--- lib/custom/setup/unittest/TablesAddTypo3TestData.php
+++ lib/custom/setup/unittest/TablesAddTypo3TestData.php
@@ -22,7 +22,7 @@ class TablesAddTypo3TestData extends \Aimeos\MW\Setup\Task\Base
 	 */
 	public function getPreDependencies()
 	{
-		return [];
+		return ['TablesCreateMShop'];
 	}
 
 
@@ -33,7 +33,7 @@ class TablesAddTypo3TestData extends \Aimeos\MW\Setup\Task\Base
 	 */
 	public function getPostDependencies()
 	{
-		return ['TablesCreateMShop'];
+		return [];
 	}
 
 
