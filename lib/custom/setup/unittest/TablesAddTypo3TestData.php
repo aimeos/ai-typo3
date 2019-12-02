d39e2bec5a5f37beecd1a61e8d715aeba311f63a
Aimeos <aimeos@aimeos.org>
2019-12-02 15:38:15 +0100

    Use DBAL to create tables required for tests

--- lib/custom/setup/unittest/TablesAddTypo3TestData.php
+++ lib/custom/setup/unittest/TablesAddTypo3TestData.php
@@ -2,8 +2,7 @@
 
 /**
  * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
- * @copyright Metaways Infosystems GmbH, 2011
- * @copyright Aimeos (aimeos.org), 2014-2018
+ * @copyright Aimeos (aimeos.org), 2015-2018
  */
 
 
@@ -11,38 +10,35 @@ namespace Aimeos\MW\Setup\Task;
 
 
 /**
- * Adds default records to tables.
+ * Creates all required TYPO3 tables.
  */
-class TablesAddTypo3TestData extends \Aimeos\MW\Setup\Task\Base
+class TablesAddTypo3TestData extends TablesCreateMShop
 {
 	/**
-	 * Returns the list of task names which depends on this task.
+	 * Returns the list of task names which this task depends on.
 	 *
-	 * @return array List of task names
+	 * @return string[] List of task names
 	 */
-	public function getPostDependencies() : array
+	public function getPreDependencies() : array
 	{
 		return ['TablesCreateMShop'];
 	}
 
 
 	/**
-	 * Adds fe_user test data.
+	 * Creates the TYPO3 test tables
 	 */
 	public function migrate()
 	{
-		$this->msg( 'Setting up Aimeos TYPO3 test tables', 0 );
-		$this->status( '' );
+		$this->msg( 'Creating TYPO3 test tables', 0, '' );
 
-		$filename = __DIR__ . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'mysql.sql';
+		$ds = DIRECTORY_SEPARATOR;
 
-		$this->msg( sprintf( 'Adding records from "%1$s"', basename( $filename ) ), 1 );
+		$this->setupSchema( ['db-customer' => 'schema' . $ds . 'customer.php'] );
 
-		if( ( $content = file_get_contents( $filename ) ) === false ) {
-			throw new \Aimeos\MW\Setup\Exception( sprintf( 'Unable to get content from file "%1$s"', $filename ) );
-		}
-
-		$this->execute( $content );
-		$this->status( 'done' );
+		$this->execute( "
+			INSERT INTO `static_countries` (`pid`, `deleted`, `cn_iso_2`, `cn_iso_3`, `cn_iso_nr`, `cn_parent_tr_iso_nr`, `cn_official_name_local`, `cn_official_name_en`, `cn_capital`, `cn_tldomain`, `cn_currency_iso_3`, `cn_currency_iso_nr`, `cn_phone`, `cn_eu_member`, `cn_address_format`, `cn_zone_flag`, `cn_short_local`, `cn_short_en`, `cn_uno_member`)
+			SELECT 0, 0, 'DE', 'DEU', 276, 155, 'Bundesrepublik Deutschland', 'Federal Republic of Germany', 'Berlin', 'de', 'EUR', 978, 49, 1, 1, 0, 'Deutschland', 'Germany', 1 FROM DUAL WHERE NOT EXISTS ( SELECT `cn_iso_2` FROM `static_countries` WHERE `cn_iso_2` = 'DE' );
+		" );
 	}
 }
---
+++ lib/custom/setup/unittest/schema/customer.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2016-2018
+ */
+
+
+return array(
+	'table' => array(
+
+		'fe_users' => function ( \Doctrine\DBAL\Schema\Schema $schema ) {
+
+			$table = $schema->createTable( 'fe_users' );
+
+			$table->addColumn( 'siteid', 'integer', ['notnull' => false] );
+			$table->addColumn( 'uid', 'integer', ['autoincrement' => true, 'unsigned' => true] );
+			$table->addColumn( 'pid', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'deleted', 'integer', ['length' => 3, 'default' => 0] );
+			$table->addColumn( 'hidden', 'integer', ['length' => 3, 'default' => 0] );
+			$table->addColumn( 'tstamp', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'crdate', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cruser_id', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'lockToDomain', 'string', ['length' => 50, 'default' => ''] );
+			$table->addColumn( 'TSconfig', 'text', ['length' => 0xffff, 'default' => ''] );
+			$table->addColumn( 'starttime', 'integer', ['length' => 11, 'unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'endtime', 'integer', ['length' => 11, 'unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'usergroup', 'string', ['length' => 0xff, 'notnull' => false] );
+			$table->addColumn( 'name', 'string', ['length' => 100, 'default' => ''] );
+			$table->addColumn( 'uc', 'text', ['length' => 0xffff, 'notnull' => false] );
+			$table->addColumn( 'username', 'string', ['length' => 50, 'notnull' => false] );
+			$table->addColumn( 'password', 'string', ['length' => 40, 'notnull' => false] );
+			$table->addColumn( 'first_name', 'string', ['length' => 50, 'notnull' => false] );
+			$table->addColumn( 'middle_name', 'string', ['length' => 50, 'notnull' => false] );
+			$table->addColumn( 'last_name', 'string', ['length' => 50, 'notnull' => false] );
+			$table->addColumn( 'address', 'string', ['length' => 255, 'notnull' => false] );
+			$table->addColumn( 'telephone', 'string', ['length' => 20, 'notnull' => false] );
+			$table->addColumn( 'fax', 'string', ['length' => 20, 'notnull' => false] );
+			$table->addColumn( 'email', 'string', ['length' => 80, 'notnull' => false] );
+			$table->addColumn( 'title', 'string', ['length' => 40, 'notnull' => false] );
+			$table->addColumn( 'city', 'string', ['length' => 50, 'notnull' => false] );
+			$table->addColumn( 'www', 'string', ['length' => 80, 'notnull' => false] );
+			$table->addColumn( 'company', 'string', ['length' => 80, 'notnull' => false] );
+			$table->addColumn( 'vatid', 'string', ['length' => 32, 'notnull' => false] );
+			$table->addColumn( 'zip', 'string', ['length' => 20, 'default' => ''] );
+			$table->addColumn( 'country', 'string', ['length' => 60, 'default' => ''] );
+			$table->addColumn( 'image', 'text', ['length' => 0xff, 'notnull' => false] );
+			$table->addColumn( 'felogin_forgotHash', 'string', ['length' => 80, 'notnull' => false] );
+			$table->addColumn( 'tx_extbase_type', 'string', ['length' => 255, 'notnull' => false] );
+			$table->addColumn( 'fe_cruser_id', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'lastlogin', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'is_online', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'date_of_birth', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'zone', 'string', ['length' => 45, 'default' => ''] );
+			$table->addColumn( 'language', 'string', ['length' => 2, 'default' => ''] );
+			$table->addColumn( 'cnum', 'string', ['length' => 50, 'default' => ''] );
+			$table->addColumn( 'token', 'string', ['length' => 32, 'default' => ''] );
+			$table->addColumn( 'gender', 'integer', ['unsigned' => true, 'default' => 99] );
+			$table->addColumn( 'status', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'by_invitation', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'module_sys_dmail_html', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'terms_acknowledged', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'tx_srfeuserregister_password', 'text', ['length' => 0xffff, 'default' => ''] );
+			$table->addColumn( 'comments', 'text', ['length' => 0xffff, 'default' => ''] );
+			$table->addColumn( 'felogin_redirectPid', 'text', ['length' => 0xff, 'default' => ''] );
+			$table->addColumn( 'static_info_country', 'string', ['length' => 3, 'default' => ''] );
+			$table->addColumn( 'longitude', 'decimal', ['precision' => 8, 'scale' => 6, 'notnull' => false] );
+			$table->addColumn( 'latitude', 'decimal', ['precision' => 8, 'scale' => 6, 'notnull' => false] );
+
+			$table->setPrimaryKey( ['uid'],'PRIMARY' );
+			$table->addIndex( ['pid'],'parent' );
+			$table->addIndex( ['username'],'username' );
+			$table->addIndex( ['is_online'],'is_online' );
+
+			return $schema;
+		},
+
+		'fe_groups' => function ( \Doctrine\DBAL\Schema\Schema $schema ) {
+
+			$table = $schema->createTable( 'fe_groups' );
+
+			$table->addColumn( 'uid', 'integer', ['autoincrement' => true, 'unsigned' => true] );
+			$table->addColumn( 'pid', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'deleted', 'integer', ['length' => 3, 'default' => 0] );
+			$table->addColumn( 'hidden', 'integer', ['length' => 3, 'default' => 0] );
+			$table->addColumn( 'tstamp', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'crdate', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cruser_id', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'title', 'string', ['length' => 50, 'default' => ''] );
+			$table->addColumn( 'lockToDomain', 'string', ['length' => 50, 'default' => ''] );
+			$table->addColumn( 'description', 'text', ['length' => 0xffff, 'default' => ''] );
+			$table->addColumn( 'TSconfig', 'text', ['length' => 0xffff, 'default' => ''] );
+			$table->addColumn( 'subgroup', 'text', ['length' => 0xff, 'default' => ''] );
+			$table->addColumn( 'felogin_redirectPid', 'text', ['length' => 0xff, 'notnull' => false] );
+			$table->addColumn( 'tx_phpunit_is_dummy_record', 'integer', ['unsigned' => true, 'length' => 1, 'default' => 0] );
+
+			$table->setPrimaryKey( ['uid'],'PRIMARY' );
+			$table->addIndex( ['pid'],'parent' );
+
+			return $schema;
+		},
+
+		'static_countries' => function ( \Doctrine\DBAL\Schema\Schema $schema ) {
+
+			$table = $schema->createTable( 'static_countries' );
+
+			$table->addColumn( 'uid', 'integer', ['autoincrement' => true, 'unsigned' => true] );
+			$table->addColumn( 'pid', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'deleted', 'integer', ['length' => 3, 'default' => 0] );
+			$table->addColumn( 'cn_iso_2', 'string', ['length' => 2, 'default' => ''] );
+			$table->addColumn( 'cn_iso_3', 'string', ['length' => 3, 'default' => ''] );
+			$table->addColumn( 'cn_iso_nr', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_parent_tr_iso_nr', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_official_name_local', 'string', ['length' => 128, 'default' => ''] );
+			$table->addColumn( 'cn_official_name_en', 'string', ['length' => 128, 'default' => ''] );
+			$table->addColumn( 'cn_capital', 'string', ['length' => 45, 'default' => ''] );
+			$table->addColumn( 'cn_tldomain', 'string', ['length' => 2, 'default' => ''] );
+			$table->addColumn( 'cn_currency_iso_3', 'string', ['length' => 3, 'default' => ''] );
+			$table->addColumn( 'cn_currency_iso_nr', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_phone', 'integer', ['unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_eu_member', 'integer', ['length' => 3, 'unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_address_format', 'integer', ['length' => 3, 'unsigned' => true, 'default' => 0] );
+			$table->addColumn( 'cn_zone_flag', 'integer', ['length' => 4, 'default' => 0] );
+			$table->addColumn( 'cn_short_local', 'string', ['length' => 70, 'default' => ''] );
+			$table->addColumn( 'cn_short_en', 'string', ['length' => 50, 'default' => ''] );
+			$table->addColumn( 'cn_uno_member', 'integer', ['length' => 3, 'unsigned' => true, 'default' => 0] );
+
+			$table->setPrimaryKey( ['uid'],'PRIMARY' );
+			$table->addIndex( ['pid'],'parent' );
+
+			return $schema;
+		},
+	),
+);
