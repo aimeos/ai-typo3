74ecc483839f87eaddefe7a20b82e9d9dda7e547
Aimeos <aimeos@aimeos.org>
2019-12-06 12:04:16 +0100

    Prevent problem when adding new columns in PostgreSQL

--- lib/custom/setup/default/schema/customer.php
+++ lib/custom/setup/default/schema/customer.php
@@ -87,7 +87,7 @@ return array(
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
 			$table->addColumn( 'parentid', 'integer', ['unsigned' => true] );
 			$table->addColumn( 'siteid', 'integer', [] );
-			$table->addColumn( 'key', 'string', array( 'length' => 134 ) );
+			$table->addColumn( 'key', 'string', array( 'length' => 134, 'default' => '' ) );
 			$table->addColumn( 'type', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'domain', 'string', array( 'length' => 32 ) );
 			$table->addColumn( 'refid', 'string', array( 'length' => 36 ) );
@@ -142,7 +142,7 @@ return array(
 			$table->addColumn( 'id', 'integer', array( 'autoincrement' => true ) );
 			$table->addColumn( 'parentid', 'integer', ['unsigned' => true] );
 			$table->addColumn( 'siteid', 'integer', [] );
-			$table->addColumn( 'key', 'string', array( 'length' => 130 ) );
+			$table->addColumn( 'key', 'string', array( 'length' => 130, 'default' => '' ) );
 			$table->addColumn( 'type', 'string', array( 'length' => 64 ) );
 			$table->addColumn( 'langid', 'string', array( 'length' => 5, 'notnull' => false ) );
 			$table->addColumn( 'value', 'string', array( 'length' => 255 ) );
