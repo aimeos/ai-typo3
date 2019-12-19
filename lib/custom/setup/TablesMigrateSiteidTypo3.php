2ea69ba0564e2c7f2fb2e6e9e3dfb3bed4a47320
Aimeos <aimeos@aimeos.org>
2019-12-19 06:18:53 +0100

    Added setup task to migrate site IDs

---
+++ lib/custom/setup/TablesMigrateSiteidTypo3.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ */
+
+
+namespace Aimeos\MW\Setup\Task;
+
+
+/**
+ * Updates site ID columns
+ */
+class TablesMigrateSiteidTypo3 extends TablesMigrateSiteid
+{
+	private $resources = [
+		'db-customer' => [
+			'fe_users_list_type', 'fe_users_property_type',
+			'fe_users_property', 'fe_users_list', 'fe_users_address', 'fe_users',
+		],
+	];
+
+
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPreDependencies() : array
+	{
+		return ['TablesMigrateSiteid'];
+	}
+
+
+	/**
+	 * Returns the list of task names which this task depends on.
+	 *
+	 * @return string[] List of task names
+	 */
+	public function getPostDependencies() : array
+	{
+		return ['TablesCreateMShop'];
+	}
+
+
+	/**
+	 * Executes the task
+	 */
+	public function migrate()
+	{
+		$this->msg( 'Update TYPO3 "siteid" columns', 0, '' );
+
+		$this->process( $this->resources );
+	}
+}
