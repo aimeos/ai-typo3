feaf51c2343b7ca1ae3631be16290f2ae5578a5f
Aimeos <aimeos@aimeos.org>
2020-01-16 02:37:44 +0100

    Avoid invalid cache identifiers

--- lib/custom/src/MAdmin/Cache/Proxy/Typo3.php
+++ lib/custom/src/MAdmin/Cache/Proxy/Typo3.php
@@ -48,7 +48,7 @@ class Typo3
 	{
 		if( !isset( $this->object ) )
 		{
-			$siteid = $this->context->getLocale()->getSiteId();
+			$siteid = $this->context->getLocale()->getSiteItem()->getId();
 			$conf = array( 'siteid' => $this->context->getConfig()->get( 'madmin/cache/prefix' ) . $siteid );
 			$this->object = \Aimeos\MW\Cache\Factory::create( 'Typo3', $conf, $this->cache );
 		}
