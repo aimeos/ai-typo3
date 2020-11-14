1d8f5cc789f802a20cb1c0fe94eb96391022cebf
Aimeos <aimeos@aimeos.org>
2020-11-14 12:26:34 +0100

    Allow array and map as parameter types for clear()

--- lib/custom/src/MShop/Customer/Manager/Group/Typo3.php
+++ lib/custom/src/MShop/Customer/Manager/Group/Typo3.php
@@ -94,7 +94,7 @@ class Typo3
 	 * @param string[] $siteids List of IDs for sites whose entries should be deleted
 	 * @return \Aimeos\MShop\Common\Manager\Iface Same object for fluent interface
 	 */
-	public function clear( array $siteids ) : \Aimeos\MShop\Common\Manager\Iface
+	public function clear( iterable $siteids ) : \Aimeos\MShop\Common\Manager\Iface
 	{
 		$path = 'mshop/customer/manager/group/submanagers';
 
