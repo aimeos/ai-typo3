50f2c0d9430082293367172c10db8c2dab81caab
Aimeos <aimeos@aimeos.org>
2021-02-02 12:26:19 +0100

    Support multiple keys to group by in aggregate()

--- lib/custom/config/mshop/customer.php
+++ lib/custom/config/mshop/customer.php
@@ -293,28 +293,28 @@ return array(
 			'typo3' => array(
 				'aggregate' => array(
 					'ansi' => '
-						SELECT "key", COUNT("id") AS "count"
+						SELECT :keys, COUNT("id") AS "count"
 						FROM (
-							SELECT :key AS "key", t3feuli."id" AS "id"
+							SELECT :acols, t3feuli."id" AS "id"
 							FROM "fe_users_list" AS t3feuli
 							:joins
 							WHERE :cond
 							ORDER BY :order
 							OFFSET :start ROWS FETCH NEXT :size ROWS ONLY
 						) AS list
-						GROUP BY "key"
+						GROUP BY :keys
 					',
 					'mysql' => '
-						SELECT "key", COUNT("id") AS "count"
+						SELECT :keys, COUNT("id") AS "count"
 						FROM (
-							SELECT :key AS "key", t3feuli."id" AS "id"
+							SELECT :acols, t3feuli."id" AS "id"
 							FROM "fe_users_list" AS t3feuli
 							:joins
 							WHERE :cond
 							ORDER BY :order
 							LIMIT :size OFFSET :start
 						) AS list
-						GROUP BY "key"
+						GROUP BY :keys
 					',
 				),
 				'delete' => array(
