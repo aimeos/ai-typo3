8ac91196cfa18f42174911f59878c3265def077f
Aimeos <aimeos@aimeos.org>
2021-04-27 18:49:22 +0200

    Use correct database for updating user site IDs

--- lib/custom/setup/TablesMigrateSiteidTypo3.php
+++ lib/custom/setup/TablesMigrateSiteidTypo3.php
@@ -54,7 +54,7 @@ class TablesMigrateSiteidTypo3 extends TablesMigrateSiteid
 		$this->process( $this->resources );
 
 		if( $this->getSchema( 'db-customer' )->columnExists( 'fe_users', 'siteid' ) !== false ) {
-			$this->execute( 'UPDATE fe_users SET siteid=\'\' WHERE siteid IS NULL' );
+			$this->execute( 'UPDATE fe_users SET siteid=\'\' WHERE siteid IS NULL', 'db-customer' );
 		}
 	}
 }
