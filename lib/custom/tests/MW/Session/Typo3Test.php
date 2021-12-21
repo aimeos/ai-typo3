f0ad19799534e19e403e162f7ff89f642f560f42
Aimeos <aimeos@aimeos.org>
2021-12-21 12:35:22 +0100

    Fixed session tests

--- lib/custom/tests/MW/Session/Typo3Test.php
+++ lib/custom/tests/MW/Session/Typo3Test.php
@@ -13,6 +13,7 @@ namespace Aimeos\MW\Session;
 class Typo3Test extends \PHPUnit\Framework\TestCase
 {
 	private $object;
+	private $mock;
 
 
 	protected function setUp() : void
@@ -21,71 +22,64 @@ class Typo3Test extends \PHPUnit\Framework\TestCase
 			$this->markTestSkipped( 'TYPO3 AbstractUserAuthentication not available' );
 		}
 
-		$mock = $this->getMockForAbstractClass( '\TYPO3\CMS\Core\Authentication\AbstractUserAuthentication' );
-		$this->object = new \Aimeos\MW\Session\Typo3( $mock );
+		$this->mock = $this->getMockBuilder( '\TYPO3\CMS\Core\Authentication\AbstractUserAuthentication' )
+			->setMethods( ['getSessionData', 'setAndSaveSessionData'] )
+			->disableOriginalConstructor()
+			->getMockForAbstractClass();
+
+		$this->object = new \Aimeos\MW\Session\Typo3( $this->mock );
 	}
 
 
 	protected function tearDown() : void
 	{
-		unset( $this->object );
+		unset( $this->object, $this->mock );
 	}
 
 
 	public function testDel()
 	{
-		$this->object->set( 'test', '123456789' );
-		$this->assertEquals( '123456789', $this->object->get( 'test' ) );
+		$this->mock->expects( $this->once() )->method( 'setAndSaveSessionData' );
 
 		$result = $this->object->del( 'test' );
 
 		$this->assertInstanceOf( \Aimeos\MW\Session\Iface::class, $result );
-		$this->assertEquals( null, $this->object->get( 'test' ) );
 	}
 
 
 	public function testGet()
 	{
-		$this->assertEquals( null, $this->object->get( 'test' ) );
+		$this->mock->expects( $this->once() )->method( 'getSessionData' )->will( $this->returnValue( '123456789' ) );
 
-		$this->object->set( 'test', '123456789' );
 		$this->assertEquals( '123456789', $this->object->get( 'test' ) );
-
-		$this->object->set( 'test', ['123456789'] );
-		$this->assertEquals( ['123456789'], $this->object->get( 'test' ) );
 	}
 
 
 	public function testPull()
 	{
-		$this->object->set( 'test', '123456789' );
-		$this->assertEquals( '123456789', $this->object->get( 'test' ) );
+		$this->mock->expects( $this->once() )->method( 'setAndSaveSessionData' );
+		$this->mock->expects( $this->once() )->method( 'getSessionData' )->will( $this->returnValue( '123456789' ) );
 
 		$this->assertEquals( '123456789', $this->object->pull( 'test' ) );
-		$this->assertEquals( null, $this->object->pull( 'test' ) );
 	}
 
 
 	public function testRemove()
 	{
-		$this->object->set( 'test', '123456789' );
-		$this->assertEquals( '123456789', $this->object->get( 'test' ) );
+		$this->mock->expects( $this->once() )->method( 'setAndSaveSessionData' );
 
 		$result = $this->object->remove( ['test'] );
 
 		$this->assertInstanceOf( \Aimeos\MW\Session\Iface::class, $result );
-		$this->assertEquals( null, $this->object->get( 'test' ) );
 	}
 
 
 	public function testSet()
 	{
-		$this->object->set( 'test', null );
-		$this->assertEquals( null, $this->object->get( 'test' ) );
+		$this->mock->expects( $this->once() )->method( 'setAndSaveSessionData' );
 
 		$result = $this->object->set( 'test', '234' );
 
 		$this->assertInstanceOf( \Aimeos\MW\Session\Iface::class, $result );
-		$this->assertEquals( '234', $this->object->get( 'test' ) );
 	}
 }
