fba4a6f3eb27e3be35d7362ad34ac96d8fc2abb4
Aimeos <aimeos@aimeos.org>
2023-01-11 11:27:48 +0100

    Copyright update

--- src/MAdmin/Cache/Proxy/Typo3.php
+++ src/MAdmin/Cache/Proxy/Typo3.php
@@ -2,7 +2,7 @@
 
 /**
  * @license LGPLv3, http://www.gnu.org/copyleft/lgpl.html
- * @copyright Aimeos (aimeos.org), 2015-2022
+ * @copyright Aimeos (aimeos.org), 2015-2023
  * @package MAdmin
  * @subpackage Cache
  */
