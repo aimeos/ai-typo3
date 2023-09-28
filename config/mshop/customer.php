36772f4347157ba1842effd8e0618135eba628be
Aimeos <aimeos@aimeos.org>
2023-09-28 09:48:08 +0200

    Added i18n column to search result

--- config/mshop/customer.php
+++ config/mshop/customer.php
@@ -245,7 +245,8 @@ return array(
 								mcuslity."code" AS "customer.lists.type.code", mcuslity."domain" AS "customer.lists.type.domain",
 								mcuslity."label" AS "customer.lists.type.label", mcuslity."status" AS "customer.lists.type.status",
 								mcuslity."mtime" AS "customer.lists.type.mtime", mcuslity."editor" AS "customer.lists.type.editor",
-								mcuslity."ctime" AS "customer.lists.type.ctime", mcuslity."pos" AS "customer.lists.type.position"
+								mcuslity."ctime" AS "customer.lists.type.ctime", mcuslity."pos" AS "customer.lists.type.position",
+								mcuslity."i18n" AS "customer.lists.type.i18n"
 							FROM "fe_users_list_type" mcuslity
 							:joins
 							WHERE :cond
@@ -258,7 +259,8 @@ return array(
 								mcuslity."code" AS "customer.lists.type.code", mcuslity."domain" AS "customer.lists.type.domain",
 								mcuslity."label" AS "customer.lists.type.label", mcuslity."status" AS "customer.lists.type.status",
 								mcuslity."mtime" AS "customer.lists.type.mtime", mcuslity."editor" AS "customer.lists.type.editor",
-								mcuslity."ctime" AS "customer.lists.type.ctime", mcuslity."pos" AS "customer.lists.type.position"
+								mcuslity."ctime" AS "customer.lists.type.ctime", mcuslity."pos" AS "customer.lists.type.position",
+								mcuslity."i18n" AS "customer.lists.type.i18n"
 							FROM "fe_users_list_type" mcuslity
 							:joins
 							WHERE :cond
@@ -461,7 +463,8 @@ return array(
 								mcusprty."code" AS "customer.property.type.code", mcusprty."domain" AS "customer.property.type.domain",
 								mcusprty."label" AS "customer.property.type.label", mcusprty."status" AS "customer.property.type.status",
 								mcusprty."mtime" AS "customer.property.type.mtime", mcusprty."editor" AS "customer.property.type.editor",
-								mcusprty."ctime" AS "customer.property.type.ctime", mcusprty."pos" AS "customer.property.type.position"
+								mcusprty."ctime" AS "customer.property.type.ctime", mcusprty."pos" AS "customer.property.type.position",
+								mcusprty."i18n" AS "customer.property.type.i18n"
 							FROM "fe_users_property_type" mcusprty
 							:joins
 							WHERE :cond
@@ -474,7 +477,8 @@ return array(
 								mcusprty."code" AS "customer.property.type.code", mcusprty."domain" AS "customer.property.type.domain",
 								mcusprty."label" AS "customer.property.type.label", mcusprty."status" AS "customer.property.type.status",
 								mcusprty."mtime" AS "customer.property.type.mtime", mcusprty."editor" AS "customer.property.type.editor",
-								mcusprty."ctime" AS "customer.property.type.ctime", mcusprty."pos" AS "customer.property.type.position"
+								mcusprty."ctime" AS "customer.property.type.ctime", mcusprty."pos" AS "customer.property.type.position",
+								mcusprty."i18n" AS "customer.property.type.i18n"
 							FROM "fe_users_property_type" mcusprty
 							:joins
 							WHERE :cond
