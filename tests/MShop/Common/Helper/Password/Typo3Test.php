11af24301dbc9b0bd04a61f8310d6c50965a848b
Aimeos <aimeos@aimeos.org>
2024-11-07 10:50:41 +0100

    Fixed test for newer PHP versions

--- tests/MShop/Common/Helper/Password/Typo3Test.php
+++ tests/MShop/Common/Helper/Password/Typo3Test.php
@@ -21,6 +21,6 @@ class Typo3Test extends \PHPUnit\Framework\TestCase
 	public function testEncode()
 	{
 		$object = new \Aimeos\MShop\Common\Helper\Password\Typo3( ['object' => \TestHelper::context()->password()] );
-		$this->assertStringStartsWith( '$2y$10$', $object->encode( 'unittest' ) );
+		$this->assertStringStartsWith( '$2y$', $object->encode( 'unittest' ) );
 	}
 }
